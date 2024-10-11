e0adf42070a2f6564efab17363ff4a3c8866de70
Joao <jm2matheus@gamil.com>
2024-10-11 02:39:52 -0300

    Mais uno

--- src/test/java/br/com/valueprojects/mock_spring/test/JogoServiceTest.java
+++ src/test/java/br/com/valueprojects/mock_spring/test/JogoServiceTest.java
@@ -29,6 +29,9 @@ public class JogoServiceTest {
 
     @Mock
     private FinalizaJogo finalizaJogo;
+    
+    @Mock
+    private Juiz juiz;
 
     @InjectMocks
     private JogoService jogoService;
@@ -42,11 +45,18 @@ public class JogoServiceTest {
         // Arrange
         Jogo jogo1 = mock(Jogo.class);
         Participante vencedor1 = mock(Participante.class);
+        Resultado resultado = mock(Resultado.class);
 
-        // Simulando que o jogo foi finalizado e iniciou na semana anterior
+        // Simulando o comportamento esperado
         when(jogo1.isFinalizado()).thenReturn(true);
         when(finalizaJogo.iniciouSemanaAnterior(jogo1)).thenReturn(true);
-        when(jogoService.vencedor(jogo1)).thenReturn(vencedor1);
+        when(juiz.getPrimeiroColocado()).thenReturn(10.0);
+        doNothing().when(juiz).julga(any(Jogo.class));
+        
+        List<Resultado> resultados = Arrays.asList(resultado);
+        when(jogo1.getResultados()).thenReturn(resultados);
+        when(resultado.getMetrica()).thenReturn(10.0);
+        when(resultado.getParticipante()).thenReturn(vencedor1);
 
         List<Jogo> jogos = Arrays.asList(jogo1);
 
@@ -64,6 +74,7 @@ public class JogoServiceTest {
         inOrder.verify(jogoDao).salva(jogo1);
         inOrder.verify(smsService).enviar(new Sms(vencedor1, "Parabéns, você venceu o jogo!"));
     }
+
 
     @Test
     public void naoDeveProcessarJogosNaoFinalizados() {
